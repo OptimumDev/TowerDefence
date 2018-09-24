d73c988c1ae913bf5eb3bfa5b11ce92f14b6111e
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-09-24 22:00:06 +0500

    images added

--- TowerDefence/GameWindow.py
+++ TowerDefence/GameWindow.py
@@ -18,7 +18,7 @@ class GameWindow(QMainWindow):
     WIDTH = 1920
     HEIGHT = 1000
 
-    SHIFT = 1
+    SHIFT = 2
 
     FONT = QFont("times", 20)
 
@@ -27,6 +27,7 @@ class GameWindow(QMainWindow):
         self.__timer = QBasicTimer()
         self.__is_paused = False
         self.game = game
+        self.coin_picture = QPixmap("images/coin.png")
         self.initUI()
 
     def initUI(self):
@@ -35,11 +36,13 @@ class GameWindow(QMainWindow):
         self.setWindowIcon(QIcon('images/smorc.png'))
 
         self.__pause_button = QPushButton('Pause/Play', self)
-        self.__pause_button.move(50, 610)
+        self.__pause_button.move(self.WIDTH - 350, 10)
         self.__pause_button.clicked.connect(self.pause_click)
 
-        self.__tower_button = QPushButton(f'Build Tower ({ArrowTower.COST}G)', self)
-        self.__tower_button.move(150, 610)
+        self.__tower_button = QPushButton('', self)
+        self.__tower_button.setStyleSheet("background: transparent;");
+        self.__tower_button.setGeometry(10, self.height() - 10 - self.IMAGE_SIZE * 5 / 2,
+                                        self.IMAGE_SIZE * 2, self.IMAGE_SIZE * 2)
         self.__tower_button.clicked.connect(self.show_tower_cell_btns)
 
         self.__restart_button = QPushButton('Restart', self)
@@ -86,7 +89,7 @@ class GameWindow(QMainWindow):
             if not cell.is_road:
                 button = QPushButton('', self)
                 button.setGeometry((cell.coordinates.x) * self.IMAGE_SIZE,
-                                   (1 + cell.coordinates.y) * self.IMAGE_SIZE,
+                                   (self.SHIFT + cell.coordinates.y) * self.IMAGE_SIZE,
                                    self.IMAGE_SIZE, self.IMAGE_SIZE)
                 button.setStyleSheet("border-image: url(images/towerButton.png) stretch;");
                 button.clicked.connect(partial(self.build_tower, button))
@@ -109,7 +112,7 @@ class GameWindow(QMainWindow):
             self.quit()
 
     def build_tower(self, button):
-        coordinates = Point(button.x(), button.y()).convert_to_cell_coordinates(self.IMAGE_SIZE, 1)
+        coordinates = Point(button.x(), button.y()).convert_to_cell_coordinates(self.IMAGE_SIZE, self.SHIFT)
         self.game.build_tower(coordinates)
         self.show_tower_cell_btns()
         self.__tower_cell_buttons.remove(button)
@@ -175,13 +178,13 @@ class GameWindow(QMainWindow):
 
     def draw_castle(self, painter):
         painter.setFont(self.FONT)
-        painter.drawText(350, 35, f'Castle health: {self.game.castle.health}')
+        painter.drawText(self.width() - 230, self.IMAGE_SIZE * 2 - 10, f'Castle health: {self.game.castle.health}')
         image = self.game.castle.image
-        coordinates = self.game.castle.coordinates.convert_to_image_coordinates(self.IMAGE_SIZE, 1)
+        coordinates = self.game.castle.coordinates.convert_to_image_coordinates(self.IMAGE_SIZE, self.SHIFT)
         painter.drawPixmap(coordinates.x, coordinates.y, self.IMAGE_SIZE * 3, self.IMAGE_SIZE * 3, image)
 
     def draw_in_cell(self, image, cell_coordinates, painter, x_shift=0, y_shift=0):
-        image_coordinates = cell_coordinates.convert_to_image_coordinates(self.IMAGE_SIZE, 1)
+        image_coordinates = cell_coordinates.convert_to_image_coordinates(self.IMAGE_SIZE, self.SHIFT)
         x = image_coordinates.x + x_shift
         y = image_coordinates.y - y_shift
         painter.drawPixmap(x, y, self.IMAGE_SIZE, self.IMAGE_SIZE, image)
@@ -191,11 +194,23 @@ class GameWindow(QMainWindow):
 
     def draw_gold(self, painter):
         painter.setFont(self.FONT)
-        painter.drawText(650, 35, f'Gold: {self.game.gold}')
+        painter.drawPixmap(20 + len(f'{self.game.gold}') * 15, self.IMAGE_SIZE // 2,
+                           self.IMAGE_SIZE, self.IMAGE_SIZE, self.coin_picture)
+        painter.drawText(10, self.IMAGE_SIZE // 2 + 40, f'{self.game.gold}')
 
-    def draw_Projectiles(self, painter):
+    def draw_projectiles(self, painter):
         for arrow in self.game.projectiles:
             self.draw_in_cell(arrow.image, arrow.coordinates, painter)
+
+    def draw_tower_costs(self, painter):
+        painter.drawPixmap(self.__tower_button.x(), self.__tower_button.y(), self.IMAGE_SIZE * 2, self.IMAGE_SIZE * 2,
+                           QPixmap('images/button.png'))
+        painter.drawPixmap(self.__tower_button.x() + 10, self.__tower_button.y() + 10,
+                           self.IMAGE_SIZE * 2 - 20, self.IMAGE_SIZE * 2 - 20, QPixmap('images/tower.png'))
+        arrow_x = self.__tower_button.x() + self.__tower_button.width() // 2 - self.IMAGE_SIZE // 2
+        arrow_y = self.__tower_button.y() + self.__tower_button.height() + 5
+        painter.drawPixmap(arrow_x - 5, arrow_y, self.IMAGE_SIZE // 2, self.IMAGE_SIZE // 2, self.coin_picture)
+        painter.drawText(arrow_x + 30, arrow_y + self.IMAGE_SIZE // 2 - 5, f'{ArrowTower.COST}')
 
     def paintEvent(self, event):
         painter = QPainter()
@@ -207,7 +222,8 @@ class GameWindow(QMainWindow):
         self.draw_towers(painter)
         self.draw_enemies(painter)
         self.draw_castle(painter)
-        self.draw_Projectiles(painter)
+        self.draw_projectiles(painter)
+        self.draw_tower_costs(painter)
 
         painter.end()
 
